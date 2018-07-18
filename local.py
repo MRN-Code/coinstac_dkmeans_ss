31bad99f4557678450827b75bd90c467799a4adf
Bradley Baker <bbradt@gmail.com>
2018-07-18 00:12:18 -0600

    updated with larger skeleton

--- local.py
+++ local.py
@@ -1,29 +1,309 @@
-import json
+import os
 import sys
-from utils import listRecursive
+import logging
+import numpy as np
+import configparser
+import local_computations as local
+
+CONFIG_FILE = 'config.cfg'
+DEFAULT_data_file = 'data.txt'
+DEFAULT_k = 5
+DEFAULT_shuffle = True
+DEFAULT_learning_rate = 0.001
+DEFAULT_optimization = 'lloyd'
 
 
-def local_1(args):
-    input_list = args["input"]
+def local_init_env(config_file=CONFIG_FILE, k=DEFAULT_k, optimization=DEFAULT_optimization, shuffle=DEFAULT_shuffle,
+                   data_file=DEFAULT_data_file, learning_rate=DEFAULT_learning_rate, **kwargs):
+    """
+        # Description:
+            Initialize the local environment, creating the config file.
 
-    output_dict = {
-        "Name": input_list["Person_Name"],
-        "computation_phase": "local_1"
-    }
-    cache_dict = {}
+        # PREVIOUS PHASE:
+            remote_init_env
 
-    computation_output = {"output": output_dict, "cache_dict": cache_dict}
+        # INPUT:
 
+            |   name            |   type    |   default     |
+            |   ---             |   ---     |   ---         |
+            |   config_file     |   str     |   config.cfg  |
+            |   k               |   int     |   5           |
+            |   optimization    |   str     |   lloyd       |
+            |   shuffle         |   bool    |   False       |
+            |   data_file       |   str     |   data.txt    |
+            |   learning_rate   |   float   |   0.001       |
+
+        # OUTPUT:
+            - config file written to disk
+
+        # NEXT PHASE:
+            local_init_centroids
+    """
+    logging.info('LOCAL: Initializing remote environment')
+    if not os.path.exists(config_file):
+        config = configparser.ConfigParser()
+        config['LOCAL'] = dict(k=k, optimization=optimization, shuffle=shuffle, data_file=data_file,
+                               learning_rate=learning_rate)
+        with open(config_file, 'w') as file:
+            config.write(file)
+    # output
+    computation_output = dict(
+        output=dict(
+            config_file=config_file,
+            computation_phase="local_init_env"
+            ),
+        success=True
+    )
+    return json.dumps(computation_output)
+
+
+def local_init_centroids(config_file=CONFIG_FILE, **kwargs):
+    """
+        # Description:
+            Initialize K centroids from own data.
+
+        # PREVIOUS PHASE:
+            local_init_env
+
+        # INPUT:
+
+            |   name             |   type    |   default     |
+            |   ---              |   ---     |   ---         |
+            |   config_file      |   str     |   config.cfg  |
+
+        # OUTPUT:
+            - centroids: list of numpy arrays
+
+        # NEXT PHASE:
+            remote_init_centroids
+    """
+    logging.info('LOCAL: Initializing centroids')
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    data = np.loadtxt(config['LOCAL']['data_file'])
+    centroids = local.initialize_own_centroids(data, config['LOCAL']['k'])
+    # output
+    computation_output = dict(
+        output=dict(
+            config_file=config_file,
+            centroids=centroids,
+            computation_phase="local_init_env"
+            ),
+        success=True
+    )
+    return json.dumps(computation_output)
+
+
+def local_compute_clustering(config_file=CONFIG_FILE, remote_centroids=None, computation_phase=None, **kwargs):
+    """
+        # Description:
+            Assign data instances to clusters.
+
+        # PREVIOUS PHASE:
+            remote_init_centroids (on first run only)
+            remote_cehck_convergence
+
+        # INPUT:
+
+            |   name                |   type    |   default     |
+            |   ---                 |   ---     |   ---         |
+            |   config_file         |   str     |   config.cfg  |
+            |   remote_centroids    |   list    |   None        |
+            |   computation_phase   |   list    |   None        |
+
+        # OUTPUT:
+            - centroids: list of numpy arrays
+
+        # NEXT PHASE:
+            remote_init_centroids
+    """
+    logging.info('LOCAL: computing clustering')
+    if remote_centroids is None:
+        raise ValueError("LOCAL: at local_compute_clustering - remote_centroids not passed correctly")
+    if computation_phase is None:
+        raise ValueError("LOCAL: at local_compute_clustering - computation_phase not passed correctly")
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    data = np.loadtxt(config['LOCAL']['data_file'])
+
+    cluster_labels = local.compute_clustering(data, remote_centroids)
+
+    new_comp_phase = "local_compute_clustering"
+    if computation_phase == "remote_optimization_step":
+        new_comp_phase = "local_compute_clustering_2"
+    computation_output = dict(
+        output=dict(
+            computation_phase=new_comp_phase,
+            cluster_labels=cluster_labels,
+            remote_centroids=remote_centroids,
+            ),
+        success=True
+    )
+    return json.dumps(computation_output)
+
+
+def local_compute_optimizer(config_file=CONFIG_FILE, remote_centroids=None, cluster_labels=None, **kwargs):
+    """
+        # Description:
+            Compute local optimizers with local data.
+
+        # PREVIOUS PHASE:
+            local_compute_clustering
+
+        # INPUT:
+
+            |   name                |   type    |   default     |
+            |   ---                 |   ---     |   ---         |
+            |   config_file         |   str     |   config.cfg  |
+            |   remote_centroids    |   list    |   None        |
+            |   cluster_labels      |   list    |   None        |
+
+        # OUTPUT:
+            - centroids: list of numpy arrays
+
+        # NEXT PHASE:
+            remote_init_centroids
+    """
+    if remote_centroids is None:
+        raise ValueError("LOCAL: at local_compute_clustering - remote_centroids not passed correctly")
+    if cluster_labels is None:
+        raise ValueError("LOCAL: at local_compute_clustering - cluster_labels not passed correctly")
+    logging.info('LOCAL: computing optimizers')
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    data = np.loadtxt(config['LOCAL']['data_file'])
+    k = config['LOCAL']['k']
+    learning_rate = config['LOCAL']['learning_rate']
+    optimization = config['LOCAL']['optimization']
+    if optimization == 'lloyd':
+        local_optimizer = local.compute_mean(data, cluster_labels, k)
+    elif optimization == 'gradient':
+        # Gradient descent has sites compute gradients locally
+        local_optimizer = \
+            local.compute_gradient(node, cluster_labels[i],
+                                   remote_centroids, learning_rate)
+    computation_output = dict(
+        output=dict(
+            local_optimizer=local_optimizer,
+            computation_phase="remote_aggregate_output"
+            ),
+        success=True
+    )
+    return json.dumps(computation_output)
+
+
+def local_optimization_step(local_centroids=None, local_optimizer=None, config_file=CONFIG_FILE):
+    """
+        # Description:
+            Use optimizer to take the next step.
+
+        # PREVIOUS PHASE:
+            local_compute_optimizer
+
+        # INPUT:
+
+            |   name             |   type    |   default     |
+            |   ---              |   ---     |   ---         |
+            |   config_file      |   str     |   config.cfg  |
+            |   remote_centroids |   list    |   config.cfg  |
+            |   remote_optimizer |   list    |   config.cfg  |
+
+        # OUTPUT:
+            - previous centroids: list of numpy arrays
+            - remote centroids: list of numpy arrays
+
+        # NEXT PHASE:
+            local_check_convergence
+    """
+
+    logging.info('LOCAL: Optimization step')
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    optimization = config['LOCAL']['optimization']
+    if optimization == 'lloyd':
+        # Then, update centroids as corresponding to the local mean
+        previous_centroids = remote_centroids[:]
+        remote_centroids = remote_optimizer[:]
+
+    elif optimization == 'gradient':
+        # Then, update centroids according to one step of gradient descent
+        [remote_centroids, previous_centroids] = \
+            local.gradient_step(remote_optimizer, remote_centroids)
+    computation_output = dict(
+        output=dict(
+            computation_phase="local_optimization_step",
+            previous_centroids=previous_centroids,
+            local_centroids=remote_centroids
+            ),
+        success=True
+    )
+    return json.dumps(computation_output)
+
+
+def local_check_convergence(local_centroids=None, previous_centroids=None, config_file=CONFIG_FILE):
+    """
+        # Description:
+            Check convergence.
+
+        # PREVIOUS PHASE:
+            local_aggregate_optimizer
+
+        # INPUT:
+
+            |   name               |   type    |   default     |
+            |   ---                |   ---     |   ---         |
+            |   config_file        |   str     |   config.cfg  |
+            |   remote_centroids   |   list    |   config.cfg  |
+            |   previous_centroids |   list    |   config.cfg  |
+
+        # OUTPUT:
+            - boolean encoded in name of phase
+
+        # NEXT PHASE:
+            remote_check_convergence
+    """
+    logging.info('LOCAL: Check convergence')
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    epsilon = config['LOCAL']['epsilon']
+    local_check, delta = local.check_stopping(local_centroids,
+                                              previous_centroids, epsilon)
+    computation_output = dict(
+        output=dict(
+            local_check=local_check,
+            delta=delta,
+            computation_phase="local_check_convergence"
+            ),
+        success=True
+    )
     return json.dumps(computation_output)
 
 
 if __name__ == '__main__':
 
     parsed_args = json.loads(sys.stdin.read())
-    phase_key = list(listRecursive(parsed_args, 'computation_phase'))
+    phase_key = list(list_recursive(parsed_args, 'computation_phase'))
 
-    if not phase_key:
-        computation_output = local_1(parsed_args)
+    if "remote_init_env" in phase_key:
+        computation_output = local_init_env(**parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif "local_init_env" in phase_key:
+        computation_output = local_init_centroids(**parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif "local_init_centroids" in phase_key:
+        computation_output = local_compute_clustering(**parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif "local_compute_clustering" in phase_key:
+        computation_output = local_compute_optimizer(**parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif "local_optimization_step" in phase_key:
+        computation_output = local_compute_clustering(**parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif 'local_compute_clustering_2' in phase_key:
+        computation_output = local_check_convergence(parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif 'remote_converged_false' in phase_key:
+        computation_output = local_compute_optimizer(**parsed_args['input'])
         sys.stdout.write(computation_output)
     else:
-        raise ValueError("Error occurred at Local")
+        raise ValueError('Phase error occurred')
--- remote.py
+++ remote.py
@@ -1,27 +1,189 @@
-import json
+"""
+Remote file for single-shot KMeans
+"""
+
+import os
 import sys
-from utils import listRecursive
+import json
+import logging
+import configparser
+import numpy as np
+import remote_computations as remote
+import local_computations as local
 
 
-def remote_1(args):
-    input_list = args["input"]
+CONFIG_FILE = 'config.cfg'
+DEFAULT_k = 5
+DEFAULT_epsilon = 0.00001
+DEFAULT_shuffle = True
+DEFAULT_learning_rate = 0.001
+DEFAULT_verbose = True
+DEFAULT_optimization = 'lloyd'
 
-    names = [input_list[site]["Name"] for site in input_list]
 
-    output_dict = {"names": names}
+def remote_init_env(config_file=DEFAULT_config_file, k=DEFAULT_k,
+                    optimization=DEFAULT_optimization, epsilon=DEFAULT_epsilon, learning_rate=DEFAULT_learning_rate,
+                    verbose=DEFAULT_verbose):
+    """
+        # Description:
+            Initialize the remote environment, creating the config file.
 
-    computation_output = {"output": output_dict, "success": True}
+        # PREVIOUS PHASE:
+            None
 
+        # INPUT:
+
+            |   name            |   type    |   default     |
+            |   ---             |   ---     |   ---         |
+            |   config_file     |   str     |   config.cfg  |
+            |   k               |   int     |   5           |
+            |   optimization    |   str     |   lloyd       |
+            |   epsilon         |   float   |   0.00001     |
+            |   shuffle         |   bool    |   False       |
+            |   data_file       |   str     |   data.txt    |
+            |   learning_rate   |   float   |   0.001       |
+            |   verbose         |   float   |   True        |
+
+        # OUTPUT:
+            - config file written to disk
+            - k
+            - learning_rate
+            - optimization
+            - shuffle
+
+        # NEXT PHASE:
+            local_init_env
+    """
+
+    logging.info('REMOTE: Initializing remote environment')
+    if not os.path.exists(config_file):
+        config = configparser.ConfigParser()
+        config['REMOTE'] = dict(k=k, optimization=optimization, epsilon=epsilon,
+                                learning_rate=learning_rate, verbose=verbose)
+        with open(config_path, 'w') as file:
+            config.write(file)
+    # output
+    computation_output = dict(output=
+                              dict(
+                                  work_dir=work_dir,
+                                  config_file=config_file,
+                                  k=k,
+                                  learning_rate=learning_rate,
+                                  optimization=optimization,
+                                  shuffle=shuffle,
+                                  computation_phase="remote_init_env"
+                                  )
+                              )
+    return json.dumps(computation_output)
+
+
+def remote_init_centroids(args, config_file=DEFAULT_config_file):
+    """
+        # Description:
+            Initialize K centroids from locally selected centroids.
+
+        # PREVIOUS PHASE:
+            local_init_centroids
+
+        # INPUT:
+
+            |   name             |   type    |   default     |
+            |   ---              |   ---     |   ---         |
+            |   config_file      |   str     |   config.cfg  |
+
+        # OUTPUT:
+            - centroids: list of numpy arrays
+
+        # NEXT PHASE:
+            local_compute_optimizer
+    """
+    logging.info('REMOTE: Initializing centroids')
+    # Have each site compute k initial clusters locally
+    local_centroids = [cent for site in args for cent in
+                       args[site]]
+    # and select k random clusters from the s*k pool
+    np.random.shuffle(local_centroids)
+    remote_centroids = local_centroids[:k]
+    computation_output = dict(
+        output=dict(
+            work_dir=work_dir,
+            config_file=config_file,
+            centroids=remote_centroids,
+            computation_phase="remote_init_centroids"
+            ),
+        success=True
+    )
+    return json.dumps(computation_output)
+
+
+def remote_check_convergence(args, config_file=CONFIG_FILE):
+    """
+        # Description:
+            Check convergence.
+
+        # PREVIOUS PHASE:
+            local_check_convergence
+
+        # INPUT:
+
+            |   name               |   type    |   default     |
+            |   ---                |   ---     |   ---         |
+            |   config_file        |   str     |   config.cfg  |
+            |   remote_centroids   |   list    |   config.cfg  |
+            |   previous_centroids |   list    |   config.cfg  |
+
+        # OUTPUT:
+            - boolean encoded in name of phase
+
+        # NEXT PHASE:
+            local_compute_clustering?
+    """
+    logging.info('REMOTE: Check convergence')
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    local_check = [site['local_check'] for site in args]
+    remote_check = any(local_check)
+    new_phase = "remote_converged_true" if remote_check else "remote_converged_false"
+    computation_output = dict(
+        output=dict(
+            computation_phase=new_phase
+            ),
+        success=True
+    )
+    return json.dumps(computation_output)
+
+
+def remote_aggregate_output(args):
+    """
+        Aggregate output. TODO: What needs to be aggregated
+    """
+    logging.info('REMOTE: Aggregating input')
+    computation_output = dict(
+        output=dict(
+            computation_phase="remote_aggregate_output"
+            ),
+        success=True
+    )
     return json.dumps(computation_output)
 
 
 if __name__ == '__main__':
 
     parsed_args = json.loads(sys.stdin.read())
-    phase_key = list(listRecursive(parsed_args, 'computation_phase'))
+    phase_key = list(list_recursive(parsed_args, 'computation_phase'))
 
-    if 'local_1' in phase_key:
-        computation_output = remote_1(parsed_args)
+    if not phase_key:
+        computation_output = remote_init_env(**parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif 'remote_aggregate_otpimizer' in phase_key:
+        computation_output = remote_optimization_step(parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif 'local_check_convergence' in phase_key:
+        computation_output = remote_check_convergence(parsed_args['input'])
+        sys.stdout.write(computation_output)
+    elif 'remote_converged_true' in phase_key:
+        computation_outut = remote_aggregate_output(parsed_args['input'])
         sys.stdout.write(computation_output)
     else:
-        raise ValueError("Error occurred at Remote")
+        raise ValueError('Oops')
+
